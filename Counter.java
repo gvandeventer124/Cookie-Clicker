80ca8b969a4250a27023737c73363e127adb8b8d
Macelai <vini.macelai@hotmail.com>
2016-06-20 19:44:08 -0300

    alpha

--- Counter.java
+++ Counter.java
@@ -1,26 +1,52 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Counter implements ActionListener {
-    private int cookie = 0;
-    private int cookiePer = 0;
+    private double cookie = 0;
+    private double cookiePer = 0, priceCursor = 10, priceGrandMa = 100, priceFarm = 1000, priceMine  = 10000, priceFactory = 100000;
+    private double priceBank = 1000000, priceTemple = 10000000, priceShipment = 100000000, priceTimeMachine = 1000000000;
+    private int numberCursor = 0, numberGrandMa = 0, numberFarm = 0, numberMine = 0, numberFactory = 0, numberBank = 0, numberTemple =0;
+    private int numberShipment = 0, numberTimeMachine = 0;
     private JLabel cookieLabel;
     private JLabel cookieLPer;
     Counter(JLabel cookieLabel, JLabel cookieLPer){
         this.cookieLabel=cookieLabel;
         this.cookieLPer=cookieLPer;
     }
+    Timer timer = new Timer();
 
     void addCokie(){
         cookie++;
     }
+
     void attCookie(){
-        cookieLabel.setText(""+cookie);
-        cookieLPer.setText(""+cookieLPer);
+        cookie=cookie+cookiePer/100;
     }
-    void attCookiePer(int multi){
-        cookiePer=cookiePer+multi;
+    void attLabel(){
+        String cookieFormat = String.format("%.0f", cookie);
+        String cookiePerFormat = String.format("%.2f", cookiePer);
+        cookieLabel.setText(cookieFormat +" Cookies");
+        cookieLPer.setText(cookiePerFormat+" Cookies/s");
+    }
+
+    public void att(){
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                attCookie();
+            }
+        },0,10);
+    }
+    public void att1(){
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                attLabel();
+            }
+        },1,1);
     }
 
 
@@ -28,8 +54,79 @@ public class Counter implements ActionListener {
         String cmd = e.getActionCommand();
         if (cmd.equals("ADD")){
             addCokie();
-        }
-        attCookie();
+        } else
+        if (cmd.equals("CURSOR")) {
+            if(cookie>=priceCursor){
+                cookie=cookie-priceCursor;
+                priceCursor=priceCursor*(1.1);
+                cookiePer=cookiePer+1;
+                numberCursor++;
+            }
+        } else
+        if (cmd.equals("GRANDMA")){
+            if(cookie >= priceGrandMa){
+                cookie=cookie-priceGrandMa;
+                priceCursor=priceGrandMa*(1.1);
+                cookiePer=cookiePer+10;
+                numberGrandMa++;
+            }
+        } else
+         if (cmd.equals("FARM")){
+             if(cookie >= priceFarm){
+                 cookie=cookie-priceFarm;
+                 priceCursor=priceFarm*(1.1);
+                 cookiePer=cookiePer+100;
+                 numberFarm++;
+             }
+         }else
+         if (cmd.equals("MINE")){
+             if(cookie >= priceMine){
+                 cookie=cookie-priceMine;
+                 priceCursor=priceMine*(1.1);
+                 cookiePer=cookiePer+1000;
+                 numberMine++;
+             }
+         }else
+         if (cmd.equals("FACTORY")){
+             if(cookie >= priceFactory){
+                 cookie=cookie-priceFactory;
+                 priceCursor=priceFactory*(1.1);
+                 cookiePer=cookiePer+10000;
+                 numberFactory++;
+             }
+         }
+         if (cmd.equals("BANK")){
+             if(cookie >= priceBank){
+                 cookie=cookie-priceBank;
+                 priceCursor=priceBank*(1.1);
+                 cookiePer=cookiePer+100000;
+                 numberBank++;
+             }
+         }else
+         if (cmd.equals("TEMPLE")){
+             if(cookie >= priceTemple){
+                 cookie=cookie-priceTemple;
+                 priceCursor=priceTemple*(1.1);
+                 cookiePer=cookiePer+1000000;
+                 numberTemple++;
+             }
+         }else
+         if (cmd.equals("SHIPMENT")){
+             if(cookie >= priceShipment){
+                 cookie=cookie-priceShipment;
+                 priceCursor=priceShipment*(1.1);
+                 cookiePer=cookiePer+10000000;
+                 numberShipment++;
+         }else
+             if (cmd.equals("TIMEMACHINE")){
+                 if(cookie >= priceTimeMachine){
+                     cookie=cookie-priceTimeMachine;
+                     priceCursor=priceTimeMachine*(1.1);
+                     cookiePer=cookiePer+100000000;
+                     numberTimeMachine++;
+                 }
+             }
+         }
     }
 }
 
--- Window.java
+++ Window.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
 
 public class Window extends JFrame {
     Window() {
@@ -10,39 +11,120 @@ public class Window extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        JPanel screen = new JPanel();
+        JLabel cookie = new JLabel("0 Cookies");
+        cookie.setFont(new Font("Monospace", Font.BOLD, 16));
+        JLabel cookieLPer = new JLabel("0 Cookies/s");
+        cookieLPer.setFont(new Font("Monospace", Font.BOLD, 12));
 
-        //setContentPane(screen);
-
-        JLabel cookie = new JLabel("0");
-        JLabel cookieLPer = new JLabel("0");
-
-        ActionListener listener = new Counter(cookie,cookieLPer);
+        Counter counter = new Counter(cookie,cookieLPer);
 
         setLayout(new BorderLayout());
-        JPanel q = new JPanel();
-        q.setLayout(new FlowLayout(FlowLayout.CENTER));
-        add(q,BorderLayout.NORTH);
-        q.add(cookie);
+        JPanel q1 = new JPanel();
+        q1.setLayout(new FlowLayout(FlowLayout.CENTER));
+        add(q1,BorderLayout.NORTH);
+        q1.add(cookie);
+        q1.setBackground(Color.white);
 
-        q = new JPanel();
-        q.setLayout(new FlowLayout(FlowLayout.CENTER));
-        add(q,BorderLayout.CENTER);
-
+        //panel do cookie grande
+        JPanel q2 = new JPanel();
+        q2.setLayout(new FlowLayout(FlowLayout.CENTER));
+        add(q2,BorderLayout.CENTER);
         ImageIcon imgCookie = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/cookie.jpg");
         JButton b = new JButton();
         b.setIcon(imgCookie);
         b.setBorder(null);
-        q.add(b);
-        b.addActionListener(listener);
+        q2.add(b);
+        b.addActionListener(counter);
         b.setActionCommand("ADD");
+        q2.setBackground(Color.white);
 
-        q = new JPanel();
-        q.setLayout(new FlowLayout(FlowLayout.TRAILING));
-        add(q,BorderLayout.SOUTH);
-        q.add(cookieLPer);
+        JPanel q3 = new JPanel();
+        q3.setLayout(new GridLayout(9,0));
+        add(q3, BorderLayout.AFTER_LINE_ENDS);
+        q3.setBackground(Color.white);
 
+        //configuração dos botões de compra
+        JButton b1 = new JButton();
+        ImageIcon imgCursor = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/CursorIcon.png");
+        b1.setIcon(imgCursor);
+        b1.setBorder(null);
+        q3.add(b1);
+        b1.addActionListener(counter);
+        b1.setActionCommand("CURSOR");
 
+        JButton b2 = new JButton();
+        ImageIcon imgGrandma = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/GrandmaIcon.png");
+        b2.setIcon(imgGrandma);
+        b2.setBorder(null);
+        q3.add(b2);
+        b2.addActionListener(counter);
+        b2.setActionCommand("GRANDMA");
+
+        JButton b3 = new JButton();
+        ImageIcon imgFarm = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/FarmIcon.png");
+        b3.setIcon(imgFarm);
+        b3.setBorder(null);
+        q3.add(b3);
+        b3.addActionListener(counter);
+        b3.setActionCommand("FARM");
+
+        JButton b4 = new JButton();
+        ImageIcon imgMine = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/MineIcon.png");
+        b4.setIcon(imgMine);
+        b4.setBorder(null);
+        q3.add(b4);
+        b4.addActionListener(counter);
+        b4.setActionCommand("MINE");
+
+        JButton b7 = new JButton();
+        ImageIcon imgFactory = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/FactoryIcon.png");
+        b7.setIcon(imgFactory);
+        b7.setBorder(null);
+        q3.add(b7);
+        b7.addActionListener(counter);
+        b7.setActionCommand("FACTORY");
+
+        JButton b5 = new JButton();
+        ImageIcon imgBank = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/BankIcon.png");
+        b5.setIcon(imgBank);
+        b5.setBorder(null);
+        q3.add(b5);
+        b5.addActionListener(counter);
+        b5.setActionCommand("BANK");
+
+        JButton b6 = new JButton();
+        ImageIcon imgTemple = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/Temple.png");
+        b6.setIcon(imgTemple);
+        b6.setBorder(null);
+        q3.add(b6);
+        b6.addActionListener(counter);
+        b6.setActionCommand("TEMPLE");
+
+        JButton b8 = new JButton();
+        ImageIcon imgShipment = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/Shipment.png");
+        b8.setIcon(imgShipment);
+        b8.setBorder(null);
+        q3.add(b8);
+        b8.addActionListener(counter);
+        b8.setActionCommand("SHIPMENT");
+
+        JButton b9 = new JButton();
+        ImageIcon imgTimeMachine = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/TimeMachine.png");
+        b9.setIcon(imgTimeMachine);
+        b9.setBorder(null);
+        q3.add(b9);
+        b9.addActionListener(counter);
+        b9.setActionCommand("TIMEMACHINE");
+
+        JPanel q4 = new JPanel();
+        q4.setLayout(new FlowLayout(FlowLayout.TRAILING));
+        add(q4,BorderLayout.SOUTH);
+        q4.add(cookieLPer);
+        q4.setBackground(Color.white);
+
+        //atualização label e cookies
+        counter.att();
+        counter.att1();
     }
 
 }
