1a729ef49ebcb2efcdba1e3ef3860ea5f0da4cea
Graham Van Deventer <gvandeventer124@gmail.com>
2017-03-08 13:17:27 -0500

    Commented/Documentation
    Added comments in order to make code more understandable to those who may copy and edit it at a later date. OR if updates to the game need to be completed

--- Window.java
+++ Window.java
@@ -5,17 +5,18 @@ import java.awt.event.ActionListener;
 import java.beans.PropertyChangeListener;
 
 public class Window extends JFrame {
-    Window() {
+    Window() { //Cookie Clicker Game Object
+        //creation of window that will contain all game objects
         setTitle("Cookie Clicker");
         setSize(500, 500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+        //Display of details involving number of cookies per second and 
         JLabel cookie = new JLabel("0 Cookies");
         cookie.setFont(new Font("Monospace", Font.BOLD, 16));
         JLabel cookieLPer = new JLabel("0 Cookies/s");
         cookieLPer.setFont(new Font("Monospace", Font.BOLD, 12));
-
+        //set details of the game itself, including background, layout of game objects, etc.
         Counter counter = new Counter(cookie,cookieLPer);
 
         setLayout(new BorderLayout());
@@ -24,12 +25,12 @@ public class Window extends JFrame {
         add(q1,BorderLayout.NORTH);
         q1.add(cookie);
         q1.setBackground(Color.white);
-
-        //panel do cookie grande
+        //creation of cookie object to be clicked on by the player
         JPanel q2 = new JPanel();
         q2.setLayout(new FlowLayout(FlowLayout.CENTER));
         add(q2,BorderLayout.CENTER);
-        ImageIcon imgCookie = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/cookie.jpg");
+        ImageIcon imgCookie = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/cookie.jpg");//Get cookie image and insert it into game as an interactable object
+        //associate the code for clicking on the cookie to the actual cookie object
         JButton b = new JButton();
         b.setIcon(imgCookie);
         b.setBorder(null);
@@ -43,7 +44,7 @@ public class Window extends JFrame {
         add(q3, BorderLayout.AFTER_LINE_ENDS);
         q3.setBackground(Color.white);
 
-        //configuração dos botões de compra
+        //Creation of button to allow player to buy additional cursor autoclicker objects
         JButton b1 = new JButton();
         ImageIcon imgCursor = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/CursorIcon.png");
         b1.setIcon(imgCursor);
@@ -51,7 +52,7 @@ public class Window extends JFrame {
         q3.add(b1);
         b1.addActionListener(counter);
         b1.setActionCommand("CURSOR");
-
+        //Creation of button to allow player to buy additional Grandma autoclicker objects
         JButton b2 = new JButton();
         ImageIcon imgGrandma = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/GrandmaIcon.png");
         b2.setIcon(imgGrandma);
@@ -59,7 +60,7 @@ public class Window extends JFrame {
         q3.add(b2);
         b2.addActionListener(counter);
         b2.setActionCommand("GRANDMA");
-
+        //Creation of button to allow player to buy additional Farm autoclicker objects
         JButton b3 = new JButton();
         ImageIcon imgFarm = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/FarmIcon.png");
         b3.setIcon(imgFarm);
@@ -67,7 +68,7 @@ public class Window extends JFrame {
         q3.add(b3);
         b3.addActionListener(counter);
         b3.setActionCommand("FARM");
-
+        //Creation of button to allow player to buy additional Mine autoclicker objects
         JButton b4 = new JButton();
         ImageIcon imgMine = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/MineIcon.png");
         b4.setIcon(imgMine);
@@ -75,7 +76,7 @@ public class Window extends JFrame {
         q3.add(b4);
         b4.addActionListener(counter);
         b4.setActionCommand("MINE");
-
+        //Creation of button to allow player to buy additional Factory autoclicker objects
         JButton b7 = new JButton();
         ImageIcon imgFactory = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/FactoryIcon.png");
         b7.setIcon(imgFactory);
@@ -83,7 +84,7 @@ public class Window extends JFrame {
         q3.add(b7);
         b7.addActionListener(counter);
         b7.setActionCommand("FACTORY");
-
+        //Creation of button to allow player to buy additional bank autoclicker objects
         JButton b5 = new JButton();
         ImageIcon imgBank = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/BankIcon.png");
         b5.setIcon(imgBank);
@@ -91,7 +92,7 @@ public class Window extends JFrame {
         q3.add(b5);
         b5.addActionListener(counter);
         b5.setActionCommand("BANK");
-
+        //Creation of button to allow player to buy additional Temple autoclicker objects
         JButton b6 = new JButton();
         ImageIcon imgTemple = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/Temple.png");
         b6.setIcon(imgTemple);
@@ -99,7 +100,7 @@ public class Window extends JFrame {
         q3.add(b6);
         b6.addActionListener(counter);
         b6.setActionCommand("TEMPLE");
-
+        //Creation of button to allow player to buy additional Rocket Shipment autoclicker objects
         JButton b8 = new JButton();
         ImageIcon imgShipment = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/Shipment.png");
         b8.setIcon(imgShipment);
@@ -107,7 +108,7 @@ public class Window extends JFrame {
         q3.add(b8);
         b8.addActionListener(counter);
         b8.setActionCommand("SHIPMENT");
-
+        //Creation of button to allow player to buy additional Time Machine autoclicker objects
         JButton b9 = new JButton();
         ImageIcon imgTimeMachine = new ImageIcon("/home/macelai/IdeaProjects/Cookie-Clicker/src/TimeMachine.png");
         b9.setIcon(imgTimeMachine);
@@ -115,14 +116,14 @@ public class Window extends JFrame {
         q3.add(b9);
         b9.addActionListener(counter);
         b9.setActionCommand("TIMEMACHINE");
-
+        
         JPanel q4 = new JPanel();
         q4.setLayout(new FlowLayout(FlowLayout.TRAILING));
         add(q4,BorderLayout.SOUTH);
         q4.add(cookieLPer);
         q4.setBackground(Color.white);
 
-        //atualização label e cookies
+        //Instantiate the timers / counters so that the game will actually operate while the player is idle
         counter.att();
         counter.att1();
     }
