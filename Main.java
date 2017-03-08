b4aee8b0559615e3a5b8dcc2560eaa306731aac7
Graham Van Deventer <gvandeventer124@gmail.com>
2017-03-08 12:39:34 -0500

    Added Documentation
    Comment has been added so code can be understood at first glance

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args){ // create a new instance of cookie clicker game
         Window window = new Window();
         window.setVisible(true);
     }
